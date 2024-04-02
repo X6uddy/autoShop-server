69180b91f7ea8feb594c7e1f71a0016d3d236ca3
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-02 16:38:59 +0400

    data model about user

---
+++ models/user-model.js
@@ -0,0 +1,12 @@
+const {Schema, model} = require('mongoose');
+
+const UserSchema = new Schema({
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    balance: {type: Number, default: 100},
+    isActivated: {type: Boolean, default: false},
+    activationLink: {type: String},
+    roles: [{type: String, ref: 'Role'}]
+})
+
+module.exports = model('User', UserSchema);
