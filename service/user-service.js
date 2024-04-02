ccdf74bf98115d4f2e2c82ba8031ef2f408f9d24
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-02 16:42:57 +0400

    functionality of interaction with the client

---
+++ service/user-service.js
@@ -0,0 +1,86 @@
+const UserModel = require('../models/user-model');
+const RoleModel = require('../models/role-model');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const MailService = require('./mail-service');
+const UserDto = require('../dtos/user-dto');
+const TokenService = require('./token-service');
+const ApiError = require('../exeptions/api-error');
+
+class UserService {
+    async registration(email, password) {
+        const candidate = await UserModel.findOne({email});
+        if(candidate) {
+            throw ApiError.BadRequest(`User ${email} уже существует`)
+        }
+        const hashPassword = await bcrypt.hash(password, 3);
+        const activationLink = uuid.v4(); 
+
+        const userId = uuid.v4();
+        const user = await UserModel.create({userId, email, password:hashPassword, activationLink, roles: ["USER"]});
+        await MailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
+
+        const userDto = new UserDto(user); //id email isActivated roles
+        const tokens = TokenService.generateTokens({...userDto});
+        await TokenService.saveToken(userDto.id, tokens.refreshToken);
+        
+
+        return {...tokens, user: userDto}
+    }
+
+    async activate (activationLink) {
+        const user = await UserModel.findOne({activationLink});
+        if(!user) {
+            throw ApiError.BadRequest('Нет такого пользователя')
+        }
+        user.isActivated = true;
+        await user.save();
+    }
+
+    async login(email, password) {
+        const user = await UserModel.findOne({email});
+        if(!user) {
+            throw ApiError.BadRequest('Пользователь не зарегистрирован');
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password); // функция которая проверит введеный пароль и захешированный(из бд)
+        if(!isPassEquals) {
+            throw ApiError.BadRequest('Неверный пароль')
+        }
+        const userDto = new UserDto(user);
+        const tokens = TokenService.generateTokens({...userDto});
+        
+        await TokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {...tokens, user: userDto}
+    }
+
+    async logout(refreshToken) {
+        const token = await TokenService.removeToken(refreshToken);
+        return token;
+    }
+
+    async refresh(refreshToken) {
+        if(!refreshToken) {
+            throw ApiError.UnathorizedError();
+        }
+
+        const userData = TokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await TokenService.findToken(refreshToken);
+        if(!userData || !tokenFromDb) {
+            throw ApiError.UnathorizedError();
+        }
+
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = TokenService.generateTokens({...userDto});
+        
+        await TokenService.saveToken(userDto.id, tokens.refreshToken);
+        return {...tokens, user: userDto}
+    }
+
+    async getAllUsers () {
+        const users = UserModel.find();
+        return users;
+    }
+}
+
+module.exports = new UserService();
