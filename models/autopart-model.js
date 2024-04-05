916f87bac5f0741dec153a02c8ec36d5e0900515
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-05 20:36:50 +0400

    autopart data model

---
+++ models/autopart-model.js
@@ -0,0 +1,14 @@
+const {Schema, model} = require('mongoose');
+
+const AutopartSchema = new Schema({
+    detailId: {type: String, required: true},
+    title: {type: String, required: true},
+    imageUrl: {type: String, required: true},
+    quantitOnHand: {type: Number, required: true},
+    price: {type: Number, required: true},
+    weight: {type: Number, required: true},
+    compatability: [{type: String, required: true}],
+    description: {type: String, required: true}
+})
+
+module.exports = model('Autopart', AutopartSchema);
