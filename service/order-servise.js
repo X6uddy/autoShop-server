7a0cd1f504aab5feb41bf5c9c6bd5d99322d9f84
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-06 12:13:56 +0400

    receiving all orders and creating a new order

---
+++ service/order-servise.js
@@ -0,0 +1,24 @@
+const AutopartModel = require('../models/autopart-model');
+const OrderModel = require('../models/order-model');
+
+class OrderServise {
+    async getAllOrders () {
+        const orders = OrderModel.find();
+        return orders;
+    }
+    async getOrdersById (clientId) {
+        const orders = OrderModel.findById({detailId});
+        return orders;
+    }
+    async makeNewOrder (details, login) {
+        const orderTime = new Date();
+        const order = await OrderModel.create({email: login, purchasedAutoParts: details, orderTime});
+
+
+
+
+        return res;
+    }
+}
+
+module.exports = new OrderServise();
