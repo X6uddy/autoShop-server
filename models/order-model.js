c46fc31770fb984cf30fdd5419b8954bbe3b7102
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-06 12:12:45 +0400

    order data model

--- models/order-model.js
+++ models/order-model.js
@@ -1,7 +1,9 @@
 const {Schema, model} = require('mongoose');
 
 const OrderSchema = new Schema({
-    
+    email: {type: String, required: true},
+    purchasedAutoParts: [{type: String, ref: 'Autopart'}],
+    orderTime: {type: Date, default: Date.now}
 })
 
 module.exports = model('Order', OrderSchema);
