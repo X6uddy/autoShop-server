dbab5b7c0cfbbd0123829778f4095c8d751695fc
Volkov Sasha <sashavolkov_03@mail.ru>
2024-04-06 12:12:56 +0400

    role data model

--- models/role-model.js
+++ models/role-model.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require('mongoose');
 
 const RoleSchema = new Schema({
-    email: {type: String, unique: true, required: true, ref: "User"},
+    email: {type: String, unique: true, required: true},
     value: {type: String, unique: true, default: "USER"}
 })
 
