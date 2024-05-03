0fc7f7d74c297f33961a73dce819ba0acbafea70
X6uddy <sashavolkov_03@mail.ru>
2024-05-04 00:25:07 +0400

    contollers for autoparts

--- controllers/autopart-controller.js
+++ controllers/autopart-controller.js
@@ -1,12 +1,31 @@
-const UserService = require('../service/user-service');
-const DetailService = require('../service/detail-service');
+const AutopartService = require('../service/autopart-service');
 
 class AutopartController {
     async createNewAutopart(req, res, next) {
         try {
-            const {newAutopart} = req.body;
-            const newDetail = await DetailService.makeNewOrder(newAutopart);
+            const {title, imageUrl, quantitOnHand, price, weight, compatability, description} = req.body;
+            const newDetail = await AutopartService.createNewAutopart(title, imageUrl, quantitOnHand, price, weight, compatability, description);
+            console.log(newDetail)
             return res.json(newDetail);
+        } catch (e) {
+            next(e)
+        }
+    }
+    async getAutopartById(req, res, next) {
+        try {
+            const {autopartId} = req.body;
+            const autopart = await AutopartService.getAutopartById(autopartId);
+            return res.json(autopart)
+        } catch (e) {
+            next(e)
+        }
+    }
+
+    async getAllAutoparts(req, res, next) {
+        try {
+            const autoparts = await AutopartService.getAllAutoparts();
+            console.log('newDetail,', autoparts)
+            return res.json(autoparts)
         } catch (e) {
             next(e)
         }
